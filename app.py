e04b1a9da7f08ec69cfc267aef9758707623aa0a
Prabhu Kiran Konda <prabhukiran8790@protonmail.com>
2023-03-11 20:50:00 +0530

    delete old models and data, add new models and data, update app.py

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ def predict_form():
             season = 2 # rainy
         if submit := st.form_submit_button("Predict"):
             inputs = np.array([T1, T2, H1, H2, season])
-            temp = predict(inputs, 'models/temp_optimal_info.jbl')
-            humidity = predict(inputs, 'models/humidity_optimal_info.jbl')
+            temp = predict(inputs, 'temp_optimal_info.jbl')
+            humidity = predict(inputs, 'humidity_optimal_info.jbl')
             st.write(f"#### Predicted Temperature: {((temp[0][0] - 32)*5)/9:.1f} °C")
             st.write(f"#### Predicted Humidity: {humidity[0][0]:.1f} %")
 
